b7be66a4a3b891c7d35ef9f9412bb4d196e76a50
JosueJSA <josuesa090@gmail.com>
2022-03-24 23:21:54 -0600

    Add project files.

---
+++ Contracts/EjemploService.cs
@@ -0,0 +1,17 @@
+﻿using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contracts
+{
+    internal class EjemploService : IEjemploService
+    {
+        public bool MostrarEjemplo(string cadena)
+        {
+            return cadena == "Entendí el ejemplo";
+        }
+    }
+}
---
+++ Services/IEjemploService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    [ServiceContract]
+    public interface IEjemploService
+    {
+        [OperationContract]
+        bool MostrarEjemplo(string cadena);
+    }
+}
